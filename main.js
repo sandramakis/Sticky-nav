121291ddba91d90415d59a9c0afcac5109eb0af1
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-11-10 20:19:10 +0100

    ADD: All files

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const nav = document.querySelector("nav");
+const topOfNav = nav.offsetTop;
+console.log(nav, topOfNav);
+
+const fixNav = () => {
+  console.log(topOfNav, window.scrollY);
+
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = `${nav.offsetHeight}px`;
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove("fixed-nav");
+  }
+};
+
+window.addEventListener("scroll", fixNav);
